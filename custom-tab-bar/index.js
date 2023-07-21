588f2b8e67478b8936b0e9ca9e2ef1d5580c0688
Austin <3165316992@qq.com>
2023-07-21 15:01:08 +0800

    修复了tabbar跳转之后图标颜色不能匹配的bug

---
+++ custom-tab-bar/index.js
@@ -0,0 +1,73 @@
+import { storeBindingsBehavior } from 'mobx-miniprogram-bindings'
+import { store } from '../store/store'
+
+// Components/tabbar/tabbar.js
+Component({
+  options: {
+    styleIsolation: 'shared'
+  },
+  behaviors: [storeBindingsBehavior],
+  storeBindings: {
+    store,
+    fields: {
+      sum: 'sum',
+      active: 'activeTabBarIndex'
+    },
+    actions: {
+      updateActive: 'updateActiveTabBarIndex'
+    },
+  },
+  observers: {
+    'sum': function (val) {
+      this.setData({
+        'list[1].info': val
+      })
+    }
+  },
+  /**
+   * 组件的属性列表
+   */
+  properties: {
+
+  },
+
+  /**
+   * 组件的初始数据
+   */
+  data: {
+    active:0,
+    "list": [{
+      "pagePath": "/pages/index/index",
+      "iconPath": "/images/icon/主页.png",
+      "selectedIconPath": "/images/icon/选中后的tabbar/主页.png",
+      "text": "主页"
+    }, {
+      "pagePath": "/pages/massage/massage",
+      "iconPath": "/images/icon/短信.png",
+      "selectedIconPath": "/images/icon/选中后的tabbar/短信.png",
+      "text": "信息"
+    },
+  {
+    "pagePath": "/pages/my/my",
+    "text": "我的",
+    "selectedIconPath": "/images/icon/选中后的tabbar/个人.png",
+    "iconPath": "/images/icon/个人.png"
+
+  }]
+  },
+
+  /**
+   * 组件的方法列表
+   */
+  methods: {
+    onChange(event){
+      
+      this.updateActive(event.detail)
+    wx.switchTab({
+      url: this.data.list[event.detail].pagePath,
+    })
+
+  
+    }
+  }
+})
---
+++ store/store.js
@@ -0,0 +1,8 @@
+import { observable, action } from 'mobx-miniprogram'
+
+export const store = observable({
+  activeTabBarIndex: 0,
+  updateActiveTabBarIndex: action(function(index) {
+    this.activeTabBarIndex = index
+  })
+})
