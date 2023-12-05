3fcb7a40a03414678437914dd5078fa859e6581e
Austin <3165316992@qq.com>
2023-12-05 18:50:24 +0800

    优化了登录注册页面

--- pages/loginAndregister/loginAndRegister.js
+++ pages/loginAndregister/loginAndRegister.js
@@ -1,12 +1,103 @@
 // pages/loginAndregister/loginAndRegister.js
 const { get, post } = require('../../utils/request');
 Page({
-
+ 
   /**
    * 页面的初始数据
    */
   data: {
-
+    checkboxValue: false,
+    loginName: '', // 登录名
+    loginPassword: '', // 登录密码
+    registerName: '', // 注册用户名
+    registerPassword: '', // 注册密码
+    confirmPassword: '', // 确认密码
+    current:1,
+  },
+  click(e){
+    let index = e.currentTarget.dataset.code;
+    this.setData({
+      current:index
+    })
+  },
+  handleCheckboxChange(event) {
+    console.log('this.data.checkboxValue ==> ' , this.data.checkboxValue);
+    this.setData({
+      // 点击之后进行取反
+      checkboxValue : !this.data.checkboxValue
+    })
+  },
+  inputChange(e) {
+    const field = e.currentTarget.dataset.field;
+    this.setData({
+      [field]: e.detail.value
+    });
+  },
+  submitLogin(e) {
+    const { loginName, loginPassword } = this.data;
+    if(this.data.checkboxValue){
+      post('/login',{
+        username:loginName,
+        password:loginPassword 
+      }).then(data => {
+        console.log('请求成功', data);
+        wx.showToast({
+          title: '登录成功',
+          icon:'none'
+        })
+        
+          wx.navigateTo({
+            url: 'pages/index/index',
+          });
+      
+        
+      })
+      .catch(error => {
+        console.log('请求失败', error);
+      });
+    }
+    else{
+      wx.showToast({
+        title: '请先同意并勾选',
+        icon:'none'
+      })
+    }
+  },
+  submitRegister(e){
+    const{ registerName,registerPassword, confirmPassword}=this.data;
+    if(registerPassword==confirmPassword&&this.data.checkboxValue){
+      post('/register',{
+        username:registerName,
+        password:registerPassword
+      }).then(data => {
+        console.log('请求成功', data);
+        wx.showToast({
+          title: '注册成功',
+          icon:'none'
+        })
+        setTimeout(function() {
+          wx.navigateTo({
+            url: '/pages/index/index',
+          });
+        }, 2000);
+        
+      })
+      .catch(error => {
+        console.log('请求失败', error);
+      });
+    }
+    else if(registerPassword!=confirmPassword){
+      wx.showToast({
+        title: '密码输入错误，请重试',
+        icon:'none'
+      })
+    }
+    else if(!this.data.checkboxValue){
+      wx.showToast({
+        title: '请先同意并勾选',
+        icon:'none'
+      })
+    }
   },
 register(){
   // post('/register',{
